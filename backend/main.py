7397fda507b687d87bb5b4a105f63f8789c18cfe
Lavanya Singh <luckysinghyt@gmail.com>
2026-08-26 09:28:02 +0530

    Adding additional middlware access

--- backend/main.py
+++ backend/main.py
@@ -59,6 +59,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:8000",
+        "https://fraud-explainer.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
